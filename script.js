43cdb7e90153d926cb9bc9f769f3e8ae5df64a65
jiajun1208 <147137834+jiajun1208@users.noreply.github.com>
2025-09-19 21:27:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // 影片列表，請將你的影片檔案放在同一個目錄下，並將檔名寫在這裡
 const videoList = [
-    'FOCS269.mp4',
+    'https://github.com/jiajun1208/SBAV2/raw/refs/heads/main/FOCS269.mp4?download=',
     'video2.mp4',
     'video3.mp4'
 ];
@@ -67,5 +67,6 @@ document.addEventListener('DOMContentLoaded', () => {
     loadVideo(currentVideoIndex);
 
 });
+
 
 
